f6141cab64890c581cb9e83674fdd3d5e9cb0d7d
Jose Flores Uribe <jfloresu@reko.td-bi>
2018-06-05 12:22:03 +0300

    Fixed tab - spaces issue

--- linearize_fasta.py
+++ linearize_fasta.py
@@ -16,98 +16,100 @@ tab_print = args.tab
 len_print = args.len
 rename_seqs_file = args.rename
 
-if rename_seqs_file != '':
+if rename_seqs_file != None:
     rename_seqs = True
     seq_counter = 0
     if rename_seqs_file == infile:
         sys.exit("Warning: This will delete the input file, please change the renamed headers mapping file.")
+else:
+    rename_seqs = False
 
 if len_print == True:
-	if tab_print == False:
-		sys.exit("Sorry --len option only valid if used with --tab.")
+    if tab_print == False:
+        sys.exit("Sorry --len option only valid if used with --tab.")
 
 with open(infile, 'rt') as fh:
-	entry = ''
-	seq = ''
-	header = ''
-        if rename_seqs == True:
-            new_header = ''
-            with open(rename_seqs_file, "wt") as renamed_seqs_fh:
-                if tab_print == False:
-                        for line in fh:
-                                if line[0] == '>':
-                                        original_header = line[1:].strip()
-                                        seq_counter += 1
-                                        new_header = ">seq_{}\n".format(seq_counter)
-                                        if entry != '':
-                                                print(entry)
-                                                entry = ''
-                                        entry += new_header
-                                        renamed_seqs_fh.write("{}\t{}".format(original_header, new_header[1:]))
-                                else:
-                                        entry += line.strip()
-                        print(entry)
-                # bc tab_print was True takes this way
-                else:
-                        if len_print == True:
-                                for line in fh:
-                                        if line[0] == '>':
-                                                if new_header != '':
-                                                        seq_len = len(seq)
-                                                        print("{0}\t{1}\t{2}".format(new_header, seq_len, seq))
-                                                        seq = ''
-                                                original_header = line[1:].strip()
-                                                seq_counter += 1
-                                                new_header = "seq_{}".format(seq_counter)
-                                                renamed_seqs_fh.write("{}\t{}\n".format(original_header, new_header))
-                                        else:
-                                                seq += line.strip()
-                                seq_len = len(seq)
-                                print("{0}\t{1}\t{2}".format(new_header, seq_len, seq))
-                        else:
-                                for line in fh:
-                                        if line[0] == '>':
-                                                if new_header != '':
-                                                        print("{0}\t{1}".format(new_header, seq))
-                                                        seq = ''
-                                                original_header = line[1:].strip()
-                                                seq_counter += 1
-                                                new_header = "seq_{}".format(seq_counter)
-                                                renamed_seqs_fh.write("{}\t{}\n".format(original_header, new_header))
-                                        else:
-                                                seq += line.strip()
-                                print("{0}\t{1}".format(new_header, seq))
-        else:
-                if tab_print == False:
-                        for line in fh:
-                                if line[0] == '>':
-                                        if entry != '':
-                                                print(entry)
-                                                entry = ''
-                                        entry += line
-                                else:
-                                        entry += line.strip()
-                        print(entry)
-                else:
-                        if len_print == True:
-                                for line in fh:
-                                        if line[0] == '>':
-                                                if header != '':
-                                                        seq_len = len(seq)
-                                                        print("{0}\t{1}\t{2}".format(header, seq_len, seq))
-                                                        seq = ''
-                                                header = line[1:].strip()
-                                        else:
-                                                seq += line.strip()
-                                seq_len = len(seq)
-                                print("{0}\t{1}\t{2}".format(header, seq_len, seq))
-                        else:
-                                for line in fh:
-                                        if line[0] == '>':
-                                                if header != '':
-                                                        print("{0}\t{1}".format(header, seq))
-                                                        seq = ''
-                                                header = line[1:].strip()
-                                        else:
-                                                seq += line.strip()
-                                print("{0}\t{1}".format(header, seq))
+    entry = ''
+    seq = ''
+    header = ''
+    if rename_seqs == True:
+        new_header = ''
+        with open(rename_seqs_file, "wt") as renamed_seqs_fh:
+            if tab_print == False:
+                    for line in fh:
+                            if line[0] == '>':
+                                    original_header = line[1:].strip()
+                                    seq_counter += 1
+                                    new_header = ">seq_{}\n".format(seq_counter)
+                                    if entry != '':
+                                            print(entry)
+                                            entry = ''
+                                    entry += new_header
+                                    renamed_seqs_fh.write("{}\t{}".format(original_header, new_header[1:]))
+                            else:
+                                    entry += line.strip()
+                    print(entry)
+            # bc tab_print was True takes this way
+            else:
+                    if len_print == True:
+                            for line in fh:
+                                    if line[0] == '>':
+                                            if new_header != '':
+                                                    seq_len = len(seq)
+                                                    print("{0}\t{1}\t{2}".format(new_header, seq_len, seq))
+                                                    seq = ''
+                                            original_header = line[1:].strip()
+                                            seq_counter += 1
+                                            new_header = "seq_{}".format(seq_counter)
+                                            renamed_seqs_fh.write("{}\t{}\n".format(original_header, new_header))
+                                    else:
+                                            seq += line.strip()
+                            seq_len = len(seq)
+                            print("{0}\t{1}\t{2}".format(new_header, seq_len, seq))
+                    else:
+                            for line in fh:
+                                    if line[0] == '>':
+                                            if new_header != '':
+                                                    print("{0}\t{1}".format(new_header, seq))
+                                                    seq = ''
+                                            original_header = line[1:].strip()
+                                            seq_counter += 1
+                                            new_header = "seq_{}".format(seq_counter)
+                                            renamed_seqs_fh.write("{}\t{}\n".format(original_header, new_header))
+                                    else:
+                                            seq += line.strip()
+                            print("{0}\t{1}".format(new_header, seq))
+    else:
+            if tab_print == False:
+                    for line in fh:
+                            if line[0] == '>':
+                                    if entry != '':
+                                            print(entry)
+                                            entry = ''
+                                    entry += line
+                            else:
+                                    entry += line.strip()
+                    print(entry)
+            else:
+                    if len_print == True:
+                            for line in fh:
+                                    if line[0] == '>':
+                                            if header != '':
+                                                    seq_len = len(seq)
+                                                    print("{0}\t{1}\t{2}".format(header, seq_len, seq))
+                                                    seq = ''
+                                            header = line[1:].strip()
+                                    else:
+                                            seq += line.strip()
+                            seq_len = len(seq)
+                            print("{0}\t{1}\t{2}".format(header, seq_len, seq))
+                    else:
+                            for line in fh:
+                                    if line[0] == '>':
+                                            if header != '':
+                                                    print("{0}\t{1}".format(header, seq))
+                                                    seq = ''
+                                            header = line[1:].strip()
+                                    else:
+                                            seq += line.strip()
+                            print("{0}\t{1}".format(header, seq))
